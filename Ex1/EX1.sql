52c08545901bbb63c8e431fb1333abfc75000d63
NicolasFMP <nicolas.costa@aluno.fmpsc.edu.br>
2026-09-11 10:43:10 -0300

    Update EX1.sql

--- Ex1/EX1.sql
+++ Ex1/EX1.sql
@@ -23,7 +23,7 @@ CREATE TABLE aulas (
 	titulo VARCHAR(255),
 	descricao TEXT,
 	link_video VARCHAR(255) NOT NULL,
-	duracao INT,
+	duracao INTERVAL,
 	id_curso INT NOT NULL,
 	
 	FOREIGN KEY (id_curso)
@@ -70,3 +70,38 @@ CREATE TABLE progresso (
 
 	UNIQUE (id_aluno, id_aula)
 );
+
+INSERT INTO instrutores (nome, email, biografia, especialidade)
+VALUES ('Nicolas', 'nicolas@email.com', 'instrutor bem legal', 'linguagem C'),
+('Vitoria', 'vitoria@email.com', 'instrutora', 'linguagem python');
+
+INSERT INTO cursos (titulo, descricao, preco, nivel, id_instrutor)
+VALUES ('Matematica', 'Aula de matematica', 500.00, 'medio', 1),
+('Física', 'Aula de física', 700.00, 'alto', 2),
+('PostgreSQL', 'Aula de PostgreSQL', 450.00, 'medio', 1);
+
+INSERT INTO aulas (titulo, descricao, link_video, duracao, id_curso)
+VALUES ('Aula 1 Matematica', 'Primeira aula de matematica', 'www.aula.com.br','02:30:00', 1),
+('Aula 2 Matematica', 'Segunda aula de matematica', 'www.aula.com.br','02:00:00', 1),
+('Aula 1 Física', 'Primeira aula de física', 'www.aula.com.br','02:00:00', 2),
+('Aula 2 Física', 'Segunda aula de física', 'www.aula.com.br','02:20:00', 2),
+('Aula 1 PostgreSQL', 'Primeira aula de PostgreSQL', 'www.aula.com.br','03:00:00', 3),
+('Aula 2 PostgreSQL', 'Segunda aula de PostgreSQL', 'www.aula.com.br','02:40:00', 3);
+
+INSERT INTO alunos (nome, email, nascimento, rua, cidade, estado, cep)
+VALUES ('Pedro', 'pedro@email.com', '2009-05-20', 'alameda', 'palhoca', 'SC', '85485-999'),
+('Amanda', 'amanda@email.com', '2000-10-02', 'rua do limao', 'palhoca', 'SC', '67676-767'),
+('Roberto', 'roberto@email.com', '1997-07-10', 'elza lucchi', 'palhoca', 'SC', '55555-555');
+
+INSERT INTO matriculas (id_aluno, id_curso, data_matricula)
+VALUES (1, 3, '2024-06-12 09:17:33'),
+(1, 1, '2024-06-12 09:19:27'),
+(2, 2, '2023-02-14 10:20:03'),
+(3, 3, '2025-03-04 08:44:29');
+
+INSERT INTO progresso (id_aluno, id_aula, data_conclusao)
+VALUES (1, 5, '2024-07-18 10:30:00'),
+(1, 1, '2024-07-20 14:20:00'),
+(1, 2, '2024-07-22 16:45:00'),
+(2, 3, '2023-03-10 09:30:00'),
+(3, 5, '2025-03-15 11:00:00');
